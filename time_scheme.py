3e823b20f66322c54d20fa3e47eb42bbbcdd943e
Yang Minghao <yangminghao@buaa.edu.cn>
2019-01-02 22:44:46 +0800

    implement time schemes

---
+++ time_scheme.py
@@ -0,0 +1,46 @@
+import abc
+
+import numpy as np
+
+
+class TimeScheme(abc.ABC):
+
+    @abc.abstractclassmethod
+    def get_u_new(self, u_old, dt):
+        pass
+
+
+class OneStepRungeKutta(TimeScheme):
+    
+    def __init__(self, rhs_func):
+        self._rhs = rhs_func
+
+    def get_u_new(self, u_old, dt):
+        return u_old + dt * self._rhs(u_old)
+
+
+class ThreeStepRungeKutta(TimeScheme):
+    def __init__(self, rhs_func):
+        self._rhs = rhs_func
+
+    def get_u_new(self, u_old, dt):
+        u_1 = u_old + dt * self._rhs(u_old)
+        u_2 = (3 * u_old + u_1 + dt * self._rhs(u_1))/4
+        u_new = (u_old + 2*(u_2 + dt * self._rhs(u_2)))/3
+        return u_new
+
+
+if __name__ == '__main__':
+    dt = 0.1
+    u_0_vec = np.array([1.0, 0.0])
+
+    def rhs(u_vec):
+        return np.exp(-u_vec)
+
+    t_marcher = OneStepRungeKutta(rhs_func=rhs)
+    u_new = t_marcher.get_u_new(u_old=u_0_vec, dt=dt)
+    print(u_new)
+    t_marcher = ThreeStepRungeKutta(rhs_func=rhs)
+    u_new = t_marcher.get_u_new(u_old=u_0_vec, dt=dt)
+    print(u_new)
+
