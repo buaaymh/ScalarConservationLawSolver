7578e6c64dbb91a6084c95549b5c316c122d9d79
Yang Minghao <yangminghao@buaa.edu.cn>
2019-01-05 20:16:02 +0800

    Implement Roe's numerical flux.

--- numerical_flux.py
+++ numerical_flux.py
@@ -1,36 +1,47 @@
 import abc
 import numpy as np
 
-from riemann_solver import RiemannSolver
 
 class NumericalFlux(abc.ABC):
     @abc.abstractmethod
-    def eval_f_vec(self, u_vec):
+    def eval_flux_vec(self, u_vec):
         pass
 
 
 class GodunovFlux(NumericalFlux):
     
     def __init__(self, riemann_solver):
-        assert riemann_solver is RiemannSolver
         self._riemann_solver = riemann_solver
 
-    def eval_f_vec(self, u_vec):
-        f = np.zeros(len(u_vec))
-        for i in range(1,len(u_vec)):
-            self._riemann_solver.set_initial(u_left=u_vec[i-1], u_right=u_vec[i+1])
-            f[i] = self._riemann_solver.eval_f_scalar_at(x_scalar=0, t_scalar=1.0)
-        # periodic condition
-        self._riemann_solver.set_initial(u_left=u_vec[-1], u_right=u_vec[0])
-        f[0] = self._riemann_solver.eval_f_scalar_at(x_scalar=0, t_scalar=1.0)
-        return f
+    def eval_flux_vec(self, u_vec):
+        flux = np.zeros(len(u_vec))
+        for i in range(len(u_vec)):
+            # u[i-1] ------ f[i-1] ------ u[i]
+            # x[i-1] ------ x[i-1/2] ---- x[i]
+            u_left = u_vec[i-1]
+            u_right = u_vec[i]
+            flux[i-1] = self._riemann_solver.eval_flux_on_t_axis(u_left=u_left, u_right=u_right)
+        return flux
 
 
 class RoeFlux(NumericalFlux):
     
-    def __init__(self):
-        pass
+    def __init__(self, flux_func):
+        self._flux_func = flux_func
 
-    def eval_f_vec(self, u_vec):
-        pass
-
+    def eval_flux_vec(self, u_vec):
+        flux_on_nodes = np.zeros(len(u_vec))
+        flux_roe = np.zeros(len(u_vec))
+        for i in range(len(u_vec)):
+            flux_on_nodes[i] = self._flux_func(u_vec[i])
+        for i in range(len(u_vec)):
+            u_left, u_right = u_vec[i-1], u_vec[i]
+            f_left, f_right = flux_on_nodes[i-1], flux_on_nodes[i]
+            if u_left < u_right:
+                f_correction = np.abs(f_right - f_left)
+            elif u_right < u_left:
+                f_correction = -np.abs(f_right - f_left)
+            else:
+                f_correction = 0.0
+            flux_roe[i-1] = (f_left + f_right - f_correction) / 2
+        return flux_roe
