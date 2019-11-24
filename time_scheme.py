1b9b8f51b9b7ab3fb1f70967ef84ca3232ec53c4
buaaymh <yangminghao@buaa.edu.cn>
2019-11-24 17:09:48 +0800

    Implement RK3 for DGM.

--- time_scheme.py
+++ time_scheme.py
@@ -1,6 +1,8 @@
 import abc
-
+from dg_limiter import WENOLimiter
 import numpy as np
+import copy
+
 
 
 class TimeScheme(abc.ABC):
@@ -41,6 +43,51 @@ class ThreeStepRungeKutta(TimeScheme):
         u_2 = (3 * u_old + u_1 + dt * self._rhs(u_1))/4
         u_new = (u_old + 2*(u_2 + dt * self._rhs(u_2)))/3
         return u_new
+
+class ThreeStepDGRungeKutta():
+    
+    def __init__(self, limiter):
+        self._limiter = limiter
+    
+    def set_rhs_func(self, rhs_func):
+        self._rhs = rhs_func
+
+    def get_cells_new(self, cells_old, dt):
+        self._cells = cells_old
+        # step 1
+        coefficient_old = self._get_coefficients(self._cells)
+        coefficient_1 = coefficient_old + dt * self._rhs(self._cells)
+        self._update(self._cells, coefficient_1)
+        self._limit(self._cells)
+        # step 2
+        coefficient_1 = self._get_coefficients(self._cells)
+        coefficient_2 = (3 * coefficient_old + coefficient_1 + dt * self._rhs(self._cells))/4
+        self._update(self._cells, coefficient_2)
+        self._limit(self._cells)
+        # step 3
+        coefficient_2 = self._get_coefficients(self._cells)
+        coefficient_new = (coefficient_old + 2*(coefficient_2 + dt * self._rhs(self._cells)))/3
+        self._update(self._cells, coefficient_new)
+        self._limit(self._cells)
+        return self._cells
+    
+    def _limit(self, cells):
+        temp_0 = copy.deepcopy(cells[-2])
+        for j in range(len(cells)):
+            temp_1 = copy.deepcopy(self._cells[j-1])
+            self._limiter.limit(temp_0,cells[j-1],cells[j])
+            temp_0 = temp_1
+    
+    def _update(self, cells, coefficients):
+        for i in range(len(cells)):
+            cells[i].set_coefficients(coefficients[i])
+    
+    def _get_coefficients(self, cells):
+        num = cells[0].get_coefficients_num()
+        coefficients = np.zeros((len(cells), num))
+        for i in range(len(cells)):
+              coefficients[i] = self._cells[i].get_coefficients()
+        return coefficients
 
 
 if __name__ == '__main__':
