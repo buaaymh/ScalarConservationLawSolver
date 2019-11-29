e4f4cde2ff44da89f400def1dacf76391bd00c58
Weicheng Pei <weicheng.pei@icloud.com>
2019-11-30 06:25:33 +0800

    Use `VtkDisplayer` as the default `Displayer`.

--- numerical_solver.py
+++ numerical_solver.py
@@ -4,7 +4,6 @@ import numpy as np
 from solver import Solver
 
 
-
 class NumericalSolver(Solver):
 
     @abc.abstractmethod
@@ -86,26 +85,27 @@ class FluxBasedSolver(NumericalSolver):
 
 if __name__ == '__main__':
     num_solver = FluxBasedSolver()
-    num_solver.set_u_0(u_0_func=lambda x: -1.0-1.0*np.sin(np.pi*x))
+    
+    num_solver.set_u_0(u_0_func=lambda x: 0.0-1.0*np.sin(np.pi*x))
 
     # Choose Numerical Flux
     from numerical_flux import GodunovFlux
     import riemann_solver
     # flux = GodunovFlux(riemann_solver.Linear(a=-1.0))
-    # flux = GodunovFlux(riemann_solver.Burgers())
+    flux = GodunovFlux(riemann_solver.Burgers())
 
     from numerical_flux import RoeFlux
     # flux = RoeFlux(flux_func=lambda x: x**2/2)
 
     from numerical_flux import FirstOrderUpwindFlux
-    flux = FirstOrderUpwindFlux(flux_func=lambda x: x**2/2, a_func=lambda x: x)
+    # flux = FirstOrderUpwindFlux(flux_func=lambda x: x**2/2, a_func=lambda x: x)
 
     num_solver.set_numerical_flux(flux)
     t_min = 0.0
     t_max = 5.0
-    t_num = 200
+    t_num = 400
     num_solver.set_range(x_min=-1.0, x_max=1.0, t_min=t_min, t_max=t_max)
-    num_solver.set_mesh(x_num=21, t_num=t_num)
+    num_solver.set_mesh(x_num=40, t_num=t_num)
 
     # Choose time scheme
     from time_scheme import OneStepRungeKutta
@@ -114,9 +114,8 @@ if __name__ == '__main__':
 
     # Display results
     x_vec = np.linspace(start=-1.0, stop=1.0, num=101)
-    t_vec = np.linspace(start=t_min, stop=t_max, num=1+t_num)
+    t_vec = np.linspace(start=t_min, stop=t_max, num=101)
     u_mat = num_solver.eval_u_matrix_at(x_vec=x_vec, t_vec=t_vec)
-    from displayer import ContourDisplayer, AnimationDisplayer
-    d_animation = AnimationDisplayer(x_vec=x_vec, t_vec=t_vec, u_mat=u_mat)
-    d_animation.display(x_min=-1.0, x_max=1.0, t_min=t_min, t_max=t_max)
-    
+    from displayer import ContourDisplayer, AnimationDisplayer, VtkDisplayer
+    displayer = VtkDisplayer(x_vec=x_vec, t_vec=t_vec, u_mat=u_mat)
+    displayer.display(x_min=-1.0, x_max=1.0, t_min=t_min, t_max=t_max)
